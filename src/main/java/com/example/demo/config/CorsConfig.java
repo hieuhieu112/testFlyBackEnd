382ee8e31c29f072774cdb9abb17a8f9ae41563c
hieuhieu112 <hieuahieua@gmail.com>
2026-09-24 23:55:54 +0700

    add git ignore

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${app.frontend-url}")
+    @Value("${spring.frontend.url}")
     private String frontendUrl;
 
     @Bean
